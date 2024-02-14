af8b3eccaf003e68ac8c457bde956f9b6ffc2dc5
Dominik <domanowiec@gmail.com>
2024-02-14 22:32:53 +0100

    update v0.3

--- src/pl/javastart/library/app/Library.java
+++ src/pl/javastart/library/app/Library.java
@@ -4,19 +4,21 @@ import pl.javastart.library.model.Book;
 
 class Library {
     public static void main(String[] args) {
-        final String appName = "Library v0.2";
+        final String appName = "Library v0.3";
 
-        Book book1 = new Book("Astrophysics for People in a Hurry", "Neil Degrasse Tyson",
+        Book[] books = new Book[1000];
+        books[0] = new Book("Astrophysics for People in a Hurry", "Neil Degrasse Tyson",
                 2017, 224, "W. W. Norton & Company", "9780393609394");
-        Book book2 = new Book("Effective Java, 3rd Edition", "Joshua Bloch", 2017,
+        books[1] = new Book("Effective Java, 3rd Edition", "Joshua Bloch", 2017,
                 416, "Addison-Wesley Professional", "9780134686097");
-        Book book3 = new Book("The Lady of the Lake", "Andrzej Sapkowski", 2017,
+        books[2] = new Book("The Lady of the Lake", "Andrzej Sapkowski", 2017,
                 560, "Orbit");
 
         System.out.println(appName);
         System.out.println("Books available in the library");
-        book1.printInfo();
-        book2.printInfo();
-        book3.printInfo();
+        books[0].printInfo();
+        books[1].printInfo();
+        books[2].printInfo();
+        System.out.println("The maximum number of books in the library is " + books.length);
     }
 }
